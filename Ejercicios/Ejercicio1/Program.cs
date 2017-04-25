23029d39ae05d8084d7089a076224a25bb3d672b
pehuen147 <pehuen-testa@hotmail.com>
2017-04-25 11:35:56 -0300

    asd

---
+++ Ejercicios/Ejercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el nombre de la pelicula");
+
+            string nombre = Console.ReadLine();
+            WebRequest req = WebRequest.Create("http://www.omdbapi.com/?t="+nombre);
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            JObject data = JObject.Parse(sr.ReadToEnd());
+
+            string titulo = (string)data["Year"];
+
+            Console.WriteLine("Esta pelicula salio en "+ titulo);
+
+            Console.ReadKey(true);
+        }
+
+    }
+}
---
+++ Ejercicios/Ejercicio2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el nombre de la ciudad");
+
+            
+            string nombre = Console.ReadLine();
+
+            string tipo;
+
+            int cont = 0;
+
+            WebRequest req = WebRequest.Create("https://maps.googleapis.com/maps/api/geocode/json?address=" + nombre);
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            JObject data = JObject.Parse(sr.ReadToEnd());
+            
+            do
+            {
+                tipo = (string)data["results"][0]["address_components"][cont]["types"][0];
+                if (tipo != "country")
+                    cont++;
+            } while (tipo != "country");
+            
+
+            string pais = (string)data["results"][0]["address_components"][cont]["long_name"];
+
+
+
+            Console.WriteLine(pais);
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
